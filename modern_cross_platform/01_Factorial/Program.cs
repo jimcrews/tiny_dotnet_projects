a44c15cdd2ec220baad64436888289832965d4dc
Jim Crews <crews.jim@gmail.com>
2021-05-17 09:18:08 +1000

    c sharp revision

---
+++ modern_cross_platform/01_Factorial/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Pass a 32-bit number to calculate and return the Factorial
+            /// </summary>
+            /// <param name="number"></param>
+            /// <returns>32-bit integer</returns>
+            static int Factorial(int number)
+            {
+                if (number < 1)
+                {
+                    return 0;
+                }
+                else if (number == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    checked // check type overflow
+                    {
+                        return number * Factorial(number - 1);
+                    }
+                }
+            }
+
+            static void RunFactorial()
+            {
+                for (var i = 1; i < 15; i++)
+                {
+                    try
+                    {
+                        System.Console.WriteLine($"{i}! = {Factorial(i):N0}");
+                    }
+                    catch (OverflowException)
+                    {
+                        System.Console.WriteLine($"{i}! is too big for a 32-bit integer.");
+                    }
+
+                }
+            }
+
+            RunFactorial();
+        }
+    }
+}
---
+++ modern_cross_platform/Instrumenting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Diagnostics;
+
+namespace Instrumenting
+{
+    /// <summary>
+    /// When running with the Debug configuration, both Debug and Trace are active and will show their output in DEBUG CONSOLE. 
+    /// When running with the Release configuration, only the Trace output is shown. 
+    /// You can therefore use 'Debug.WriteLine' calls liberally throughout your code, knowing they will be stripped out automatically 
+    /// when you build the release version of your application.
+    /// 
+    /// dotnet run --configuration Release
+    /// dotnet run --configuration Debug
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // write to a text file in the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+            // text writer is buffered, so this option calls
+            // Flush() on all listeners after writing
+            Trace.AutoFlush = true;
+
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+        }
+    }
+}
