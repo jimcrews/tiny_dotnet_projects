5ee58be977e1133e6a8929f29961b5d75333e17f
Jim Crews <crews.jim@gmail.com>
2021-11-07 13:01:03 +1100

    add proj

---
+++ HelloServiceStack/HelloServiceStack.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+using System;
+using ServiceStack;
+using HelloServiceStack.ServiceModel;
+
+namespace HelloServiceStack.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
